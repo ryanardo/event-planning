12eaca044e74567abc837e3c09509cbb319b7b17
Ryan G Ferris <ryanglenferris@gmail.com>
2018-01-11 13:49:57 -0800

    test6.1-passes
    Create getPriceFood getter method and test to assure it passes.

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -4,19 +4,22 @@ public class Event {
 
     private Integer guest; // number of guests to accommodate for
     private String food; // type of meal per guest; course_meal_x3 $50/per, course_meal_x7 $70/per, course_meal_x10 $100/per
-    private int priceFood;
+    private Integer priceFood;
     private String beverage; // open_bar $100/guest, one_round $10/guest, byob $0/guest
-    private int priceBeverage;
+    private Integer priceBeverage;
     private String entertainment; // dj_muzik_person $100 000/event, ipod_playlist_on_shuffle $1 000/event, nickleback $0/event
-    private int priceEntertainment;
-    private int price; // price for the event; will equal the amount of 'guest * food + guest * beverage + entertainment'
+    private Integer priceEntertainment;
+    private Integer price; // price for the event; will equal the amount of 'guest * food + guest * beverage + entertainment'
 
     public Event(int guest, String food, String beverage, String entertainment) {
         this.guest = guest;
         this.food = food;
         this.beverage = beverage;
         this.entertainment = entertainment;
-        price = 0;
+        this.priceFood = 0;
+        this.priceBeverage = 0;
+        this.priceEntertainment = 0;
+        this.price = 0;
     }
 
     public int getGuest() {
@@ -33,5 +36,17 @@ public class Event {
 
     public String getEntertainment() {
         return entertainment;
+    }
+
+    public int getPriceFood() {
+        if (this.food.equals("x3")) {
+            return priceFood = 50;
+        } else if (this.food.equals("x7")) {
+            return priceFood = 70;
+        } else if (this.food.equals("x10")) {
+            return priceFood = 100;
+        } else {
+            return priceFood = 0;
+        }
     }
 }
