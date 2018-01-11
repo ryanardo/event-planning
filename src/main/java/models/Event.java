461fa28ecb59a129d20011951127c5ceb291ce40
Ryan G Ferris <ryanglenferris@gmail.com>
2018-01-11 13:39:32 -0800

    test6.1-fails
    Update getFoodPrice getter test, and run test to assure it fails.

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -4,8 +4,11 @@ public class Event {
 
     private Integer guest; // number of guests to accommodate for
     private String food; // type of meal per guest; course_meal_x3 $50/per, course_meal_x7 $70/per, course_meal_x10 $100/per
+    private int priceFood;
     private String beverage; // open_bar $100/guest, one_round $10/guest, byob $0/guest
+    private int priceBeverage;
     private String entertainment; // dj_muzik_person $100 000/event, ipod_playlist_on_shuffle $1 000/event, nickleback $0/event
+    private int priceEntertainment;
     private int price; // price for the event; will equal the amount of 'guest * food + guest * beverage + entertainment'
 
     public Event(int guest, String food, String beverage, String entertainment) {
@@ -22,7 +25,6 @@ public class Event {
 
     public String getFood() {
         return food;
-
     }
 
     public String getBeverage() {
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -18,37 +18,37 @@ public class EventTest {
 
     @Test
     public void newEvent_instantiatesCorrectly() throws Exception {
-        Event testEvent = new Event (100, "course_meal_x10", "one_round", "nickleback");
+        Event testEvent = new Event (100, "", "one_round", "nickleback");
         assertEquals(true, testEvent instanceof Event);
     }
 
     @Test
     public void newEvent_getGuest_100() {
-        Event testEvent = new Event(100, "course_meal_x10", "one_round", "nickleback");
+        Event testEvent = new Event(100, "x10", "one_round", "nickleback");
         assertEquals(100, testEvent.getGuest());
     }
 
     @Test
-    public void newEvent_getFood_course_meal_x10() {
-        Event testEvent = new Event(100, "course_meal_x10", "one_round", "nickleback");
-        assertEquals("course_meal_x10", testEvent.getFood());
+    public void newEvent_getFood_x10() {
+        Event testEvent = new Event(100, "x10", "one_round", "nickleback");
+        assertEquals("x10", testEvent.getFood());
     }
 
     @Test
     public void newEvent_getBeverage_one_round() {
-        Event testEvent = new Event(100, "course_meal_x10", "one_round", "nickleback");
+        Event testEvent = new Event(100, "x10", "one_round", "nickleback");
         assertEquals("one_round", testEvent.getBeverage());
     }
 
     @Test
     public void newEvent_getEntertainment() {
-        Event testEvent = new Event(100, "course_meal_x10", "one_round", "nickleback");
+        Event testEvent = new Event(100, "x10", "one_round", "nickleback");
         assertEquals("nickleback", testEvent.getEntertainment());
     }
 
     @Test
-    public void newEvent_priceFood_10K() {
-        Event testEvent = new Event(100, "course_meal_x10", "one_round", "nickleback");
-        assertEquals(10000, testEvent.getPriceFood);
+    public void newEvent_getPriceFood_100() {
+        Event testEvent = new Event(100, "x10", "one_round", "nickleback");
+        assertEquals(100, testEvent.getPriceFood());
     }
 }
