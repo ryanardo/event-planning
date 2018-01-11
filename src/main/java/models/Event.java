6aabaadd90bf616c92e09dd9efa0a2c65bf012ef
Ryan G Ferris <ryanglenferris@gmail.com>
2018-01-11 11:31:39 -0800

    event-planning
    Create the event-planning project.

---
+++ src/main/java/models/Event.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Event {
+
+    public Integer guest;
+    public String food;
+    public String beverage;
+    public String entertainment;
+
+    public Event() {
+        this.guest = guest;
+        this.food = food;
+        this.beverage = beverage;
+        this.entertainment = entertainment;
+    }
+}
---
+++ src/test/java/models/EventTest.java
@@ -0,0 +1,18 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
